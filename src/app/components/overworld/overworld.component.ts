1ff1f5ed1e01020b066156fb1a37fe3aaace95e1
Matt H <mhh129@gmail.com>
2022-12-27 12:37:07 -0500

    Add overworld-map-01.svg background image

--- src/app/components/overworld/overworld.component.ts
+++ src/app/components/overworld/overworld.component.ts
@@ -1,38 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition,
-} from '@angular/animations';
 import { GameObject } from 'src/app/classes/game-object.class';
 
 @Component({
   selector: 'app-overworld',
   templateUrl: './overworld.component.html',
   styleUrls: ['./overworld.component.scss'],
-  // animations: [
-  //   trigger('openClose', [
-  //     state('open', style({
-  //       height: '200px',
-  //       opacity: 1,
-  //       backgroundColor: 'yellow'
-  //     })),
-  //     state('closed', style({
-  //       height: '100px',
-  //       opacity: 0.8,
-  //       backgroundColor: 'blue'
-  //     })),
-  //     transition('open => closed', [
-  //       animate('1s')
-  //     ]),
-  //     transition('closed => open', [
-  //       animate('0.5s')
-  //     ]),
-  //   ]),
-  // ],
 })
 export class OverworldComponent implements OnInit {
   canvas: HTMLCanvasElement | null = null;
