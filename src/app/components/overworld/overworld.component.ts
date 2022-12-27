e0c9b56544a23bdf7c0f59b15d9eef27ebf21bb9
Matt H <mhh129@gmail.com>
2022-12-27 11:41:23 -0500

    Create overworld component

---
+++ src/app/components/overworld/overworld.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-overworld',
+  templateUrl: './overworld.component.html',
+  styleUrls: ['./overworld.component.scss']
+})
+export class OverworldComponent {
+
+}
