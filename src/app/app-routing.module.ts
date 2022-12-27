063c5cc0899f8311c604a1e02acc0c46f71e9b3d
Matt H <mhh129@gmail.com>
2022-12-27 11:43:11 -0500

    Create dashboard & login components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { OpenWorldComponent } from './components/open-world/open-world.component';
+
+const routes: Routes = [
+  { path: 'battlefield', component: BattlefieldComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'open-world', component: OpenWorldComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes,
+      { enableTracing: true }
+    ),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { OverworldComponent } from './components/overworld/overworld.component';
 import { OverworldMapComponent } from './components/overworld-map/overworld-map.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     OverworldComponent,
-    OverworldMapComponent
+    OverworldMapComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
