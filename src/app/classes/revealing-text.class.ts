02e232555e85873445a5ef4ea9d7daae5f9337bd
Matt H <mhh129@gmail.com>
2022-12-29 14:02:23 -0500

    Part 10 - create revealing-text class, implement 'typewriter' effect for text messages

---
+++ src/app/classes/revealing-text.class.ts
@@ -0,0 +1,47 @@
+export class RevealingText {
+  element;
+  text: string;
+  speed: number;
+  timeout: any = null;
+  isDone: boolean;
+
+  constructor(config: any) {
+    this.element = config.element;
+    this.text = config.text;
+    this.speed = config.speed || 70;
+
+    this.timeout = null;
+    this.isDone = false;
+  }
+
+  init(): void {
+    let characters: any[] = [];
+    this.text.split('').forEach(character => {
+
+      // Create an individual <span> for each character & add them all to the DOM
+      let span = document.createElement('span');
+      span.textContent = character;
+      this.element.appendChild(span);
+
+      // Add each <span> to our internal state, ie. the 'characters' array
+      characters.push({
+        span,
+        delayAfter: character === ' ' ? 0 : this.speed,
+      });
+    });
+    this.revealOneCharacter(characters);
+  }
+
+  revealOneCharacter(list: any[]) {
+    const next = list.splice(0, 1)[0];
+    next.span.classList.add('revealed');
+
+    if (list.length > 0) {
+      this.timeout = setTimeout(() => {
+        this.revealOneCharacter(list);
+      }, next.delayAfter);
+    } else {
+      this.isDone = true;
+    }
+  }
+}
--- src/app/classes/text-message.class.ts
+++ src/app/classes/text-message.class.ts
@@ -1,38 +1,51 @@
 import { KeyPressListener } from "./key-press-listener.class";
+import { RevealingText } from "./revealing-text.class";
 
 export class TextMessage {
   text: string;
   onComplete: any;
   element: any; // append to DOM to show text message
   actionListener: any;
+  revealingText: any;
 
   constructor({text, onComplete}: any) {
     this.text = text;
     this.onComplete = onComplete;
   }
 
+  init(container: any) {
+    this.createElement();
+    container.appendChild(this.element);
+    this.revealingText.init();
+  }
+
   createElement() {
+
+    // Create a container <div> to hold text messages
     this.element = document.createElement('div');
     this.element.classList.add('TextMessage');
 
     this.element.innerHTML = (`
-      <p class="TextMessage_p">${this.text}</p>
+      <p class="TextMessage_p"></p>
       <button mat-button class="TextMessage_button">Next</button>
     `);
 
+    // Initialize the 'typewriter' effect
+    this.revealingText = new RevealingText({
+      element: this.element.querySelector('.TextMessage_p'),
+      text: this.text,
+    })
+
+    // Create a button that can be used to close the text message box
     this.element.querySelector('button').addEventListener('click', () => {
       this.done();
     });
 
+    // Configure the 'Enter' button to close the text message box
     this.actionListener = new KeyPressListener('Enter', () => {
       this.actionListener.unbind();
       this.done();
     })
-  }
-
-  init(container: any) {
-    this.createElement();
-    container.appendChild(this.element);
   }
 
   // Close the text message
