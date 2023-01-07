c59258abf898437cc2ab8e0a3836ddf3c53ee545
Matt H <mhh129@gmail.com>
2023-01-07 16:32:54 -0500

    Part 15 - create progress class

---
+++ src/app/classes/progress.class.ts
@@ -0,0 +1,52 @@
+import { playerState } from "../shared/player-state";
+
+export class Progress {
+  mapId;
+  startingHeroX;
+  startingHeroY;
+  startingHeroDirection;
+  saveFileKey;
+  
+  constructor() {
+    this.mapId = "Street";
+    this.startingHeroX = 0;
+    this.startingHeroY = 0;
+    this.startingHeroDirection = "down";
+    this.saveFileKey = "PizzaLegends_SaveFile1";
+  }
+
+  save() {
+    window.localStorage.setItem(this.saveFileKey, JSON.stringify({
+      mapId: this.mapId,
+      startingHeroX: this.startingHeroX,
+      startingHeroY: this.startingHeroY,
+      startingHeroDirection: this.startingHeroDirection,
+      playerState: {
+        pizzas: playerState.pizzas,
+        lineup: playerState.lineup,
+        items: playerState.items,
+        storyFlags: playerState.storyFlags
+      }
+    }))
+  }
+
+  getSaveFile() {
+    const file = window.localStorage.getItem(this.saveFileKey);
+    
+    return file ? JSON.parse(file) : null
+  }
+  
+  load() {
+    const file = this.getSaveFile();
+    if (file) {
+      this.mapId = file.mapId;
+      this.startingHeroX = file.startingHeroX;
+      this.startingHeroY = file.startingHeroY;
+      this.startingHeroDirection = file.startingHeroDirection;
+      Object.keys(file.playerState).forEach(key => {
+        (playerState as any)[key] = file.playerState[key];
+      })
+    }
+  }
+
+}
