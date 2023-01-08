530853d7485bb61b45c984c646d284598c1dbb27
Matt H <mhh129@gmail.com>
2023-01-08 06:44:06 -0500

    Update title-screen stylesheet

--- src/app/classes/keyboard-menu.class.ts
+++ src/app/classes/keyboard-menu.class.ts
@@ -23,8 +23,8 @@ export class KeyboardMenu {
 
     // This is an alternative way to call *.appendChild() on either this.descriptionContainer
     // or container
-    (this.descriptionContainer || container).appendChild(this.descriptionElement);
     container.appendChild(this.element);
+    (this.descriptionContainer || container).appendChild(this.descriptionElement);
 
     // Note - there is a bug where prevButton references the 'Go back' button from previous
     // menu submission selections, ex. click Items -> Go back and the submission menu will
@@ -62,9 +62,6 @@ export class KeyboardMenu {
 
   setOptions(options: any) {
     this.options = options;
-
-    console.log('--- options:', options);
-    
     this.element.innerHTML = this.options.map((option, index) => {
 
       // TODO - fix edge case where a button can be both disabled and receive auto focus
@@ -85,32 +82,29 @@ export class KeyboardMenu {
     }).join('');
 
     this.element.querySelectorAll('button').forEach((button: any) => {
-      button.addEventListener('click', () => {
 
-        // Note: button.dataset.button references the data-button attribute on the <button> 
-        // element
+      // Note: button.dataset.button references the data-button attribute on the <button> 
+      // element
+      button.addEventListener('click', () => {
         const chosenOption = this.options[ Number(button.dataset.button) ];
         chosenOption.handler();
       });
-      button.addEventListener('mouseenter', () => {
 
-        // Note -- this adds the user agent stylesheet :focus-visible { outline: ... }
-        // to buttons, which is basically a button border
+      // Note: button.focus() adds :focus-visible { outline: ... } via the user 
+      // agent stylesheet to buttons, and is basically a button border
+      button.addEventListener('mouseenter', () => {
         button.focus();
       });
       button.addEventListener('focus', () => {
         this.prevFocus = button;
-
-        // Note: button.dataset.description references the data-description attribute on the 
-        // <button> element
         this.descriptionElementText.innerText = button.dataset.description;
       });
     });
 
     // Focus on the first <button> element with the data-button attribute that is not disabled
     setTimeout(() => {
-      const focusButtonDisasbled = this.element.querySelector("button:not([disabled])").focus();
       // const focusButton = this.element.querySelector("button[data-button]:not([disabled])").focus();
+      const focusButtonDisasbled = this.element.querySelector("button:not([disabled])").focus();
     });
   }
 
@@ -121,7 +115,7 @@ export class KeyboardMenu {
     // Create a description box that displays descriptions for each menu option
     this.descriptionElement = document.createElement('div');
     this.descriptionElement.classList.add('DescriptionBox');
-    this.descriptionElement.innerHTML = [`<p>Descriptions go here</p>`];
+    this.descriptionElement.innerHTML = [`<p></p>`];
     this.descriptionElementText = this.descriptionElement.querySelector('p');
   }
 
